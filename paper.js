29212af2d64ba224d0a03dbaefc6f63dcef0aed0
greeshma1627 <68261180+greeshma1627@users.noreply.github.com>
2020-09-08 15:15:20 -0700

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+
+class paper {
+    constructor(x, y,) {
+       var options = {
+        'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      
+      
+    
+      this.body = Bodies.circle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, ball);
+    }
+    display() {
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      image(this.paper,20,20,this.width,this.height);
+      this.paper = loadImage('downlaod.jpg');
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(CENTER);
+      strokeWeight(4);
+      stroke("green");
+      fill(255);
+      rect(0, 0, this.width, this.height);
+    }
+  };
+  
